1c79536ff1fd2fcdc3c76e3762be7904ab59b2be
Luis Rafael Matiz López <149787952+Luis-MATIZ@users.noreply.github.com>
2026-01-05 23:44:54 +0100

    restructuring of the project and launch of app 2

---
+++ APPS/fl_components/lib/screens/listview1_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Listview1Screen extends StatelessWidget {
+  const Listview1Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('Listview1Screen')));
+  }
+}
